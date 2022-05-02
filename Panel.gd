15c9bb0ef51161624c04384887941c99c1734319
vetustaivy <102760235+vrojasosorio@users.noreply.github.com>
2022-05-02 18:24:07 -0400

    Implementación Timer
    1. Implementado funcionalmente timer para time left
    2. Cuando el contador llega a 0, se pregunta si se quiere reintentar, si clickea el botón, se recarga la escena actual

---
+++ Panel.gd
@@ -0,0 +1,17 @@
+extends Panel
+
+# _delta, no se utilizará para nada
+func _process(_delta):
+	#muestra el tiempo en minutos:segundos
+	$TimeLabel.text = "Tiempo Restante: %d:%02d" % [floor($TimeLeft.time_left / 60), int($TimeLeft.time_left) % 60]
+
+
+func _on_TimeLeft_timeout():
+	get_tree().paused = true
+	$Button.visible = true
+
+
+
+func _on_Button_pressed():
+	get_tree().paused = false
+	get_tree().reload_current_scene()

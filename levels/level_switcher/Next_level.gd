f3650e820e88af560bbb81b304d15df5c444d127
vijo30 <101999975+vijo30@users.noreply.github.com>
2022-05-07 00:09:06 -0400

    Renombres, Fondo y Next_level
    Cambié los nombres de las escenas  y las moví a la carpeta levels con el objetivo de generalizar el cambio de nivel, de tal forma que no sea necesario hardcodearlo en cada nivel, es decir, colocas la escena Next_level en la carpeta levels, en tu nivel, y te cambia de nivel al hacer contacto con el player, siempre y cuando sigas el formato de nombre de escena "Level_#" donde # es el número del nivel, además tienen que estar en la carpeta levels o sino no funciona. Además agregué un background a los niveles.

---
+++ levels/level_switcher/Next_level.gd
@@ -0,0 +1,8 @@
+extends Sprite
+
+signal entered
+
+func _on_Hitbox_area_entered(area):
+	emit_signal("entered")
+	print("entered")
+	get_tree().change_scene("res://levels/Level_" + str(int(get_tree().current_scene.name) + 1) + ".tscn")
--- player/Player.gd
+++ player/Player.gd
@@ -2,42 +2,47 @@ extends KinematicBody2D
 
 export var move_speed := 50
 export var gravity := 2000
+
+var velocity := Vector2.ZERO
+
 # Grounded
 var grounded = false
 # Enable gravity
 var grv = true
 # Direction of climb_wall
 var lastDir = 0
-# Climbing
+
+# Climbing var
 var climb = false
-var velocity := Vector2.ZERO
+
+# Attack var
 var canIAttack = false
-var running = false
-var idle = false
 var attacking = false
 onready var timer = get_node("Timer")
+onready var next_level = get_tree().get_root()
 
-func _ready():
+# Activate animations
+func _ready(): 
 	$AnimationTree.active = true
+	next_level.connect("entered", self, "handleNextLevel")
+
+# Change the levels
+func handleNextLevel():	
+	print("res://levels/Level_" + str(int(get_tree().current_scene.name) + 1) + ".tscn")
+
 
 # INNOVATIVE MECHANICS
 func _on_GroundDetector_body_entered(body):
-	if body.is_in_group('tilemap'):
-		grounded = true
-
+	if body.is_in_group('tilemap'): grounded = true
 
 func _on_GroundDetector_body_exited(body):
-	if body.is_in_group('tilemap'):
-		grounded = false
+	if body.is_in_group('tilemap'): grounded = false
 
 func _on_climb_wall_body_entered(body):
-	if body.is_in_group('tilemap'):
-		climb = true
-
+	if body.is_in_group('tilemap'): climb = true
 
 func _on_climb_wall_body_exited(body):
-	if body.is_in_group('tilemap'):
-		climb = false
+	if body.is_in_group('tilemap'): climb = false
 
 func _process(delta):
 	if weakref(Manager.progress_bar).get_ref():
@@ -45,9 +50,9 @@ func _process(delta):
 			canIAttack = true
 			# sonidito, cambio color en la barra, shader al player
 
-
+# Character
 func _physics_process(delta):
-
+	# Horizontal movement
 	# reset horizontal velocity
 	velocity.x = 0
 	
@@ -58,69 +63,56 @@ func _physics_process(delta):
 	if mve > 0: $HeroKnight.flip_h = false
 	if mve < 0: $HeroKnight.flip_h = true
 	# set direction of climb_wall	
-	if mve != 0:
-		lastDir = mve
-		running = true
+	if mve != 0: lastDir = mve
 	$climb_wall.rotation_degrees = lastDir * 90
 	
+	# Vertical movement	
 	grv = not climb
 	if climb: velocity.y = 0
+	
 	# apply gravity
-	# player always has downward velocity
-	if grv:
-		velocity.y += gravity * delta
+	if grv: velocity.y += gravity * delta
 	
 	# move upwards or downwards on walls
-	if Input.is_action_pressed("move_up") and (climb):
-		velocity.y -= move_speed
-	if Input.is_action_pressed("move_down") and (climb):
-		velocity.y += move_speed
+	if Input.is_action_pressed("move_up") and (climb): velocity.y -= move_speed
+	if Input.is_action_pressed("move_down") and (climb): velocity.y += move_speed
 
 
 	# actually move the player
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	
 	# animations	
-	if mve==0:
-		if not attacking:
-			$AnimationTree.set("parameters/movement/current",0)
+	if mve==0 and (not attacking): $AnimationTree.set("parameters/movement/current",0)
 	
 	if abs(mve)>0 and not climb:
-		running = true
+		attacking = false
 		$AnimationTree.set("parameters/movement/current",1)
 	
 	if climb:
-		if velocity.y == 0:
-			$AnimationTree.set("parameters/on_wall/current",0)
-		else:
-			$AnimationTree.set("parameters/on_wall/current",1)
+		if velocity.y == 0: $AnimationTree.set("parameters/on_wall/current",0)
+		else: $AnimationTree.set("parameters/on_wall/current",1)
 	
-	if not climb and not grounded:
-		$AnimationTree.set("parameters/in_air_state/current",1)
+	if not climb and not grounded: $AnimationTree.set("parameters/in_air_state/current",1)
 	
-	if climb:
-		$AnimationTree.set("parameters/in_air_state/current",2)
+	if climb: $AnimationTree.set("parameters/in_air_state/current",2)
 		
-	if grounded:
-		$AnimationTree.set("parameters/in_air_state/current",0)
+	if grounded: $AnimationTree.set("parameters/in_air_state/current",0)
 
 	# attack implementation
-	if Input.is_action_just_pressed("attack") and canIAttack:
+	if Input.is_action_pressed("attack") and canIAttack:
 		timer.start()
 		canIAttack = false
 		attacking = true
 		$AnimationTree.set("parameters/movement/current",2)
 	
-func _on_Area2D_body_entered(_body):
-	# when the Player reaches this area, it completes the level (Changes the scene to the next one)
-	# self.position.x = 484
-	# self.position.y = 166
-	get_tree().change_scene("res://SecondLevel.tscn")
+
 	
 func _input(event):
-	if event.is_action_pressed("exit_game"):
-		get_tree().quit()
+	if event.is_action_pressed("exit_game"): get_tree().quit()
+
+
+func _on_Timer_timeout(): attacking = false
 
 
 
-func _on_Timer_timeout():
-	attacking = false

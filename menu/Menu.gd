3e7ab7aba80faca4023dae37586675933cf5fb0c
vetustaivy <102760235+vrojasosorio@users.noreply.github.com>
2022-05-18 00:07:11 -0400

    Option Menu
    - Agregué funcionalidades al Option Button:
    
    * Fullscreen Mode
    * Done Button para volver al Menu principal

--- menu/Menu.gd
+++ menu/Menu.gd
@@ -1,10 +1,22 @@
 extends Control
 
 func _ready():
-	$VBoxContainer/StartButton.grab_focus()
+	$GameMenu/StartButton.grab_focus()
 
 func _on_StartButton_pressed():
 		get_tree().change_scene("res://levels/Level_1.tscn")
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+
+func _on_FullscreenButton_pressed():
+	if OS.is_window_fullscreen():
+		OS.set_window_fullscreen(false)
+	else:
+		OS.set_window_fullscreen(true)
+
+
+func _on_Switch_pressed():
+	$GameMenu.visible = !$GameMenu.visible
+	$GameSettings.visible = !$GameSettings.visible

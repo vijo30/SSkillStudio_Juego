ec46641eaddfa5b9908f6a2c8f47ecdbe36d7de8
vetustaivy <102760235+vrojasosorio@users.noreply.github.com>
2022-07-04 23:20:50 -0400

    New Enemie: Callampin

---
+++ enemies/static/Callampin.gd
@@ -0,0 +1,77 @@
+extends KinematicBody2D
+
+signal health_updated(health)
+signal killed()
+
+var direction = 1
+export var max_health = 1
+onready var health = max_health setget _set_health
+
+var attacking = false
+var damaged = false
+var dead = false
+var alert = false # player en rango de ataque
+
+func _ready():
+	$AnimatedCallampin.set_animation("idle")
+	
+func _physics_process(_delta):
+	if not dead:
+		detect_turn_around()
+		try_to_attack()
+
+func detect_turn_around():
+	print($CallampinBack.get_collider())
+	if $CallampinBack.get_collider() != null:
+		print("hola")
+		if $CallampinBack.get_collider().is_in_group("Player"):
+			print("en la espalda tengo al weon")
+			# volteamos al callampin completo
+			scale.x = -scale.x
+			direction *= -1
+
+func try_to_attack():
+	# si no lo he atacado y está en mi rango, me lo piteo
+	if alert == true and attacking ==false:
+		$AnimatedCallampin.set_animation("attack")
+		$attackTimer.start()
+		attacking = true
+
+# pa volver a atacar
+func _on_attackTimer_timeout():
+	attacking = false
+
+func _set_health(value):
+	var prev_health = health
+	health = clamp(value, 0, max_health)
+	if health != prev_health:
+		emit_signal("health_updated",health)
+		print(str(health))
+		if health == 0:
+			die()
+			emit_signal("killed")
+
+func die():
+	dead = true
+	$CallampinCollision.set_deferred("disabled",true)
+	$AttackRangeArea/AttackRange.set_deferred("disabled",true)
+	$CallampinBack.set_deferred("disabled",true)
+	$AnimatedCallampin.set_animation("death")
+
+func _on_AttackRangeArea_body_entered(body):
+	if body.is_in_group("Player"):
+		alert = true
+		print("ALERTA")
+
+func _on_AttackRangeArea_body_exited(body):
+	if body.is_in_group("Player"):
+		alert = false
+
+func _on_Hitbox_area_entered(area):
+	if area.is_in_group("sword"):
+		die()
+
+# aqui vuelve al estado normal despues de atacar
+func _on_AnimatedCallampin_animation_finished():
+	if $AnimatedCallampin.animation == "attack":
+		$AnimatedCallampin.set_animation("idle")
